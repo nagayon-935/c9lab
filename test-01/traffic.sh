16b66d9572c2a4a3d7b22fcceb75256e3fe56973
nagayon-935 <92247537+nagayon-935@users.noreply.github.com>
2024-12-18 05:15:01 +0900

    edit host name

--- test-01/traffic.sh
+++ test-01/traffic.sh
@@ -3,35 +3,35 @@
 set -eu
 
 startTraffic1-2() {
-    echo "starting traffic between clients 1 and 2"
-    sudo docker exec client2 bash /config/iperf.sh
+    echo "starting traffic between pcs 1 and 2"
+    sudo docker exec pc02 bash /config/iperf.sh
 }
 
 startTraffic1-3() {
-    echo "starting traffic between clients 1 and 3"
-    sudo docker exec client3 bash /config/iperf.sh
+    echo "starting traffic between pcs 1 and 3"
+    sudo docker exec pc03 bash /config/iperf.sh
 }
 
 startAll() {
-    echo "starting traffic on all clients"
-    sudo docker exec client2 bash /config/iperf.sh
-    sudo docker exec client3 bash /config/iperf.sh
+    echo "starting traffic on all pcs"
+    sudo docker exec pc02 bash /config/iperf.sh
+    sudo docker exec pc03 bash /config/iperf.sh
 }
 
 stopTraffic1-2() {
-    echo "stopping traffic between clients 1 and 2"
-    sudo docker exec client2 pkill iperf3
+    echo "stopping traffic between pcs 1 and 2"
+    sudo docker exec pc02 pkill iperf3
 }
 
 stopTraffic1-3() {
-    echo "stopping traffic between clients 1 and 3"
-    sudo docker exec client3 pkill iperf3
+    echo "stopping traffic between pcs 1 and 3"
+    sudo docker exec pc03 pkill iperf3
 }
 
 stopAll() {
     echo "stopping all traffic"
-    sudo docker exec client2 pkill iperf3
-    sudo docker exec client3 pkill iperf3
+    sudo docker exec pc02 pkill iperf3
+    sudo docker exec pc03 pkill iperf3
 }
 
 # start traffic
