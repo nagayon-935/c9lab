630a08724ec8c58d6899dc863b56359374a9eb37
nagayon-935 <92247537+nagayon-935@users.noreply.github.com>
2025-01-07 17:31:06 +0900

    Delete test-env directory

--- test-env/configs/pc02/iperf.sh
+++
@@ -1,4 +0,0 @@
-# Start iperf3 server in the background
-# with 8 parallel tcp streams, each 200 Kbit/s == 1.6Mbit/s
-# using ipv6 interfaces
-iperf3 -c 192.168.1.1 -t 10000 -i 1 -p 5201 -B 172.16.2.1 -P 8 -b 200K -M 1460 &
