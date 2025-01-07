501bcd2e7960373626d3b0f91745452f58081206
nagayon-935 <92247537+nagayon-935@users.noreply.github.com>
2025-01-07 21:14:37 +0900

    init

---
+++ test-01/traffic.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -eu
+
+startTraffic1-2() {
+    echo "starting traffic between pcs 1 and 2"
+    sudo docker exec pc02 bash /config/iperf.sh
+}
+
+startTraffic1-3() {
+    echo "starting traffic between pcs 1 and 3"
+    sudo docker exec pc03 bash /config/iperf.sh
+}
+
+startAll() {
+    echo "starting traffic on all pcs"
+    sudo docker exec pc02 bash /config/iperf.sh
+    sudo docker exec pc03 bash /config/iperf.sh
+}
+
+stopTraffic1-2() {
+    echo "stopping traffic between pcs 1 and 2"
+    sudo docker exec pc02 pkill iperf3
+}
+
+stopTraffic1-3() {
+    echo "stopping traffic between pcs 1 and 3"
+    sudo docker exec pc03 pkill iperf3
+}
+
+stopAll() {
+    echo "stopping all traffic"
+    sudo docker exec pc02 pkill iperf3
+    sudo docker exec pc03 pkill iperf3
+}
+
+# start traffic
+if [ $1 == "start" ]; then
+    if [ $2 == "1-2" ]; then
+        startTraffic1-2
+    fi
+    if [ $2 == "1-3" ]; then
+        startTraffic1-3
+    fi
+    if [ $2 == "all" ]; then
+        startAll
+    fi
+fi
+
+if [ $1 == "stop" ]; then
+    if [ $2 == "1-2" ]; then
+        stopTraffic1-2
+    fi
+    if [ $2 == "1-3" ]; then
+        stopTraffic1-3
+    fi
+    if [ $2 == "all" ]; then
+        stopAll
+    fi
+fi
